92f57cb690fac2452fe644600fab56c7719312ee
TzuChieh <james093131@gmail.com>
2020-09-26 15:36:16 +0800

    ADD_Each_Iteration_Convergence

--- SAfunction.hpp
+++ SAfunction.hpp
@@ -3,10 +3,82 @@
 #define A 0.4
 #define temperature 1
 #include "normal.hpp"
-#include<math.h>
-void NeighborSelection(char *,int );
-double SAformula(int   ,int );
-double randomminmax();
-void update(double ,double ,char *,char *,int ,int &,int ,double &);
-void SA(char *,int &,int ,int ,double);
-void run(char *,int &,int ,int ,double ,int ,int *);
+
+c1d NeighborSelection(c1d arr,int size){
+	int ran=rand()%size;
+	if(arr[ran]=='1')
+		arr[ran]='0';
+	else
+		arr[ran]='1';
+
+    return arr;
+}
+double SAformula(int newsol  ,int currsol)
+{
+    double pa=exp((newsol-currsol)/temperature);
+    return pa;
+}
+double randomminmax()
+{
+    double r = (randomnum_max - randomnum_min) * rand() / (RAND_MAX + 1.0) + randomnum_min;
+    return r;
+}
+c1d update(double r,double pa,c1d newarr,c1d currarr,int len,int &opt,int curropt,double &T)
+{
+	T=A*T;
+    if(pa>r)
+    {
+		
+		opt=curropt;
+        return newarr;
+	}
+    else{
+        return currarr;
+    }
+}
+void SA(c1d arr,int &opt,int iteration,int len,double T)
+{
+		int k=0;
+		while(k<iteration)
+		{
+			c1d temp(len);
+			for(int i=0;i<len;i++)
+			{
+				temp[i]=arr[i];
+			}
+			temp = NeighborSelection(temp,len);
+            int curropt=Evaluate(temp,len);
+            double pa =SAformula(curropt,opt);
+            double r=randomminmax();
+            arr = update(r,pa,temp,arr,len,opt,curropt,T);
+			cout<<"iteration"<<k+1<<":"<<opt<<endl;
+            DRAW[k] += opt;
+			// if(opt==len)
+			// 	break;			
+			k++;
+		}
+}
+void run(c1d arr,int &opt,int iteration,int len,double T,int run,i1d result)
+{
+	int r=0;
+	while(r<run)
+	{
+		cout<<len<<"bits"<<endl;
+		c1d   arr(len);
+		arr = create(arr,len);
+		opt=Evaluate(arr,len);
+        cout<<opt<<endl;
+		SA(arr,opt,iteration,len,T);
+		result[r]=opt;
+		r++;
+	}
+    int avgiteration=avg(result,run);
+    DRAW = i1davg(DRAW,run);
+	cout<<"Average Optima :"<<avgiteration<<endl;
+	fstream file;//寫檔
+	file.open("SAresult.txt",ios::out);
+    for(int i=0;i<DRAW.size();i++)
+    {
+        file<<i<<' '<<DRAW[i]<<endl;
+    }
+}
--- normal.hpp
+++ normal.hpp
@@ -5,11 +5,111 @@
 #include<stdlib.h>
 #include <string.h>
 #include<time.h>
+#include<vector>
+#include<math.h>
 using namespace std;
-void create(char *,int );
-void Print(char *,int );
-int Evaluate(char *,int );
-void add(char *,int );
-void sub (char *,int );
-int avg(int *,int );
-void compare(char *,char *,int ,int &,int );
+
+
+typedef vector<char> c1d;
+typedef vector<int> i1d;
+
+
+i1d DRAW;
+
+c1d create(c1d arr,int size)
+{
+	for(int i=0;i<size;i++)
+	{
+  	int a=rand()%2;
+    char c =a+'0';
+    arr[i]=c;
+    }
+    return arr;
+}
+ void Print(c1d arr,int size){
+	for(int i=0;i<size;i++)
+	{
+		cout<<arr[i]<<' ';
+	}
+	cout<<endl;
+} 
+int Evaluate(c1d arr,int size)
+{
+	int optimal=0;
+	for(int i=0;i<size;i++)
+	{
+		if(arr[i]=='1')
+			optimal++;
+	}
+	return optimal;
+}
+void add(c1d arr,int len)
+{
+	int i=len-1;
+	int chc=1;
+	while(chc){
+    	if(arr[i]=='1')
+		{
+			arr[i]='0';
+			i--;
+		}
+		else if (arr[i]=='0')
+		{
+			arr[i]='1';
+			chc=0;
+			break;
+		}
+	} 
+}
+void sub (c1d arr,int len)
+{
+	int i=len-1;
+	int chc=1;
+	while(chc){
+    	if(arr[i]=='1')
+		{
+			arr[i]='0';
+			chc=0;
+			break;
+		}
+		else if (arr[i]=='0')
+		{
+			arr[i]='1';
+			i--;
+		}
+	} 
+}
+int avg(i1d arr,int len)
+{
+	int sum=0;
+	for(int i=0;i<len;i++)
+	{
+		sum+=arr[i];
+		cout<<"RUN"<<i+1<<':'<<arr[i]<<endl;
+	}
+	int avg=sum/len;
+	return avg;
+}
+i1d i1davg(i1d arr,int run)
+{
+	i1d sum;
+    sum.resize(arr.size());
+	for(int i=0;i<arr.size();i++)
+	{
+		sum [i]= arr[i]/run;
+	}
+	return sum;
+}
+void compare(c1d arr,c1d temp,int len,int &opt,int curropt)
+{
+	if(curropt>=opt)
+	{
+		for(int i=0;i<len;i++)
+		{
+			arr[i]=temp[i];
+		}
+		opt=curropt;
+	}
+
+	
+}
